798f9e5dc4cab408c7575e9ad200439023fd24d5
Iskander-glitch <iskander007007@gmail.com>
2026-05-03 15:16:40 +0100

    Implement plan rendering and deletion functionality

---
+++ plans.js
@@ -0,0 +1,27 @@
+function renderPlans() {
+const storedPlans = JSON.parse(localStorage.getItem("plans")) || plans
+const tbody = document.getElementById("plans-tbody")
+tbody.innerHTML = ""
+}
+storedPlans.forEach((plan, index) => {
+tbody.innerHTML += `
+<tr>
+<td>${plan.name}</td>
+<td>${plan.price} DA</td>
+<td>${plan.duration} month(s)</td>
+<td>${plan.features.join(", ")}</td>
+<td>
+<button onclick="deletePlan(${index})">Delete</button>
+</td>
+</tr>
+`
+})
+Exported with AI Exporter
+4 / 9
+function deletePlan(index) {
+const storedPlans = JSON.parse(localStorage.getItem("plans")) || plans
+storedPlans.splice(index, 1)
+localStorage.setItem("plans", JSON.stringify(storedPlans))
+renderPlans()
+}
+renderPlans()
